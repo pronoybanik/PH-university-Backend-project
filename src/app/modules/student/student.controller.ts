ffa10c2c4dc5f2c2a7c7a9661334ef1227120e82
PRONOY <pronoybanik82@gmail.com>
2024-12-02 12:10:25 +0600

    setup hire order function in controller

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,67 +1,43 @@
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { StudentServices } from './student.service';
 import sendResponse from '../../utils/sendResponst';
 import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
 
-const getAllStudents = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const result = await StudentServices.getAllStudentsFromDB();
+const getAllStudents = catchAsync(async (req, res, next) => {
+  const result = await StudentServices.getAllStudentsFromDB();
 
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Student are retrieved successfully',
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student are retrieved successfully',
+    data: result,
+  });
+});
 
-const getSingleStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const studentId = req.params.id;
-    const result = await StudentServices.getSingleStudentsFromDB(studentId);
+const getSingleStudent = catchAsync(async (req, res, next) => {
+  const studentId = req.params.id;
+  const result = await StudentServices.getSingleStudentsFromDB(studentId);
 
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Student are rerived successfully',
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student are rerived successfully',
+    data: result,
+  });
+});
 
-const deleteStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const studentId = req.params.id;
+const deleteStudent = catchAsync(async (req, res, next) => {
+  const studentId = req.params.id;
+  const result = await StudentServices.deletedStudentsFromDB(studentId);
 
-    const result = await StudentServices.deletedStudentsFromDB(studentId);
-
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Student deleted succesfully',
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student deleted succesfully',
+    data: result,
+  });
+});
 
 export const StudentControllers = {
   getAllStudents,
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,28 +1,19 @@
+import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponst';
 import { UserService } from './user.service';
-import { NextFunction, Request, Response } from 'express';
-import httpStatus from 'http-status';
+ import httpStatus from 'http-status';
 
-const createStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const { Student: studentData, password } = req.body;
+const createStudent = catchAsync(async (req, res, next) => {
+  const { Student: studentData, password } = req.body;
+  const result = await UserService.createStudentIntoBD(studentData, password);
 
-    const result = await UserService.createStudentIntoBD(studentData, password);
-
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Student create succesfully',
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student create succesfully',
+    data: result,
+  });
+});
 
 export const UserController = {
   createStudent,
